ad2515fc8375813b874cfcce67faf17b360476f0
nraju-panw <54722053+nraju-panw@users.noreply.github.com>
2019-10-23 16:02:29 -0700

    Add files via upload

---
+++ test.tf
@@ -0,0 +1,11 @@
+ resource  "aws_s3_bucket" "publics3" {
+      acl = "${access_type}"
+      bucket = "publics3"
+      tags  {
+        Environment = "Public"
+        Name = "My bucket"
+      }
+}
+
+
+
